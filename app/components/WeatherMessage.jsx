515c80ba3f5d9298e42599ff44a0fe2f4a4dbeca
John Deegan <john.deegan@hotmail.com>
2016-09-06 00:51:20 +0100

    Initial commit

---
+++ app/components/WeatherMessage.jsx
@@ -0,0 +1,29 @@
+var React = require('react');
+
+/*var WeatherMessage = React.createClass({
+  render: function () {
+    var temp = this.props.temp;
+    var location = this.props.location;
+
+    return (
+      <h3>It is {temp} in {location}</h3>
+    )
+  }
+});*/
+
+/*var WeatherMessage = (props) => {
+  var {temp, location} = props;
+
+  return (
+    <h3>It is {temp} in {location}</h3>
+  )
+};*/
+
+var WeatherMessage = ({temp, location}) => {
+  return (
+    <h3>It is {temp} in {location}</h3>
+  )
+};
+
+
+module.exports = WeatherMessage;
