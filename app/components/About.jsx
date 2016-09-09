9a0cf08b956ff57c8583380ce2081bc3de5e6c69
John Deegan <john.deegan@hotmail.com>
2016-09-09 14:30:30 +0100

    added css

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,7 +11,7 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h1 className="text-center">About</h1>
+      <h1 className="text-center page-title">About</h1>
       <p>
         This is an application built on React, this is for the udemy React course.
       </p>
